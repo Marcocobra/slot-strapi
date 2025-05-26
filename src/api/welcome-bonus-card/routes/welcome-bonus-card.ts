fc568b087848dc0005ed14cb3804a561e7e565b6
Imran <mahmud.bubt.150@gmail.com>
2025-05-26 10:00:34 +0600

    added welcome bonus card

---
+++ src/api/welcome-bonus-card/routes/welcome-bonus-card.ts
@@ -0,0 +1,7 @@
+/**
+ * welcome-bonus-card router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::welcome-bonus-card.welcome-bonus-card');
---
+++ src/api/welcome-bonus-card/services/welcome-bonus-card.ts
@@ -0,0 +1,7 @@
+/**
+ * welcome-bonus-card service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::welcome-bonus-card.welcome-bonus-card');
